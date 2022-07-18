63011eb502ec801a344597b3033a8da0d6d32414
Alex <asenges400@gmail.com>
2022-07-17 21:33:43 -0300

    @asenges 2022-07-17 21:33:43

--- binary_trees.h
+++ binary_trees.h
@@ -7,7 +7,6 @@
 #include <stddef.h>
 /**
  * struct binary_tree_s - Binary tree node
- *
  * @n: Integer stored in the node
  * @parent: Pointer to the parent node
  * @left: Pointer to the left child node
